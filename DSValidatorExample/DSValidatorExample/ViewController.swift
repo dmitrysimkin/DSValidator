85a8a06dc1022db8c863a17195ee5aa55f869e29
Dmitry Simkin <d.simkin@godeltech.com>
2020-05-17 00:57:15 +0300

    Added Example project

---
+++ DSValidatorExample/DSValidatorExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DSValidatorExample
+//
+//  Created by Dzmitry Simkin on 5/17/20.
+//  Copyright © 2020 Dzmitry Simkin. All rights reserved.
+//
+
+import UIKit
+import DSValidator
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+}
+
