1596cf1f5b87dcec5f8c7b4446bac74e5bee9c4c
Dmitry Simkin <d.simkin@godeltech.com>
2020-05-22 20:02:07 +0300

    Document Syntax enum

--- Sources/DSValidator/Models/Syntax/Syntax.swift
+++ Sources/DSValidator/Models/Syntax/Syntax.swift
@@ -6,14 +6,24 @@
 //  Copyright © 2020 Dzmitry Simkin. All rights reserved.
 //
 
+/// String syntax validation enum
 public enum Syntax {
+    /// Email
     case email
+    /// Person's first name
     case name
+    /// HTTP url
     case http
+    /// File ulr
     case file
+    /// Webscket url
     case webSocket
+    /// IP v4 address
     case IPv4
+    /// IP v6 address
     case IPv6
+    /// Domain name
     case domain
+    /// Geographic coordinates
     case geoCoordinate
 }
